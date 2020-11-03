1105ed86a419b7c24069246d6f1077b0bf43cff4
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-11-03 17:57:52 +0100

    new scanner overlay (made in casper)

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,7 @@
    <footer>
-      <div id="footer-nav">
-         <nav class="footer-nav">
-            <a href="handlers/logoff.php" style="color:white;">Log ud</a>
-         </nav>
-      </div>
+      <nav>
+         <a href="handlers/logoff.php" style="color:white;">Log ud</a>
+      </nav>
    </footer>
       <!-- axios -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js" integrity="sha512-quHCp3WbBNkwLfYUMd+KwBAgpVukJu5MncuQaWXgCrfgcxCJAq/fo+oqrRKOj+UKEmyMCG3tb8RB63W+EmrOBg==" crossorigin="anonymous"></script>
