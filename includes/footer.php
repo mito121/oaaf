8d694492080bd9c16d73d5726ccc22c162452e86
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-11-04 13:23:40 +0100

    menu-toggle added

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
    <footer>
-      <nav>
-         <a href="handlers/logoff.php" style="color:white;">Log ud</a>
-      </nav>
+      <div id="footer-menu">
+         <div class="nav-toggle"></div>
+         <!--<a href="handlers/logoff.php" style="color:white;">Log ud</a>-->
+      </div>
    </footer>
       <!-- axios -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js" integrity="sha512-quHCp3WbBNkwLfYUMd+KwBAgpVukJu5MncuQaWXgCrfgcxCJAq/fo+oqrRKOj+UKEmyMCG3tb8RB63W+EmrOBg==" crossorigin="anonymous"></script>
