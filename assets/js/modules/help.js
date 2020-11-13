ea18558b1b4011d6e563465e37418ae9c21b2766
Zoinkeren <60384695+Zoinkeren@users.noreply.github.com>
2020-11-13 10:13:12 +0100

    asd

--- assets/js/modules/help.js
+++ assets/js/modules/help.js
@@ -1,23 +1,23 @@
 // Submit help overlay
 let overlayOpen = false;
-document.getElementById('helpForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (overlayOpen == true) {
-        document.getElementById('helpOverlay').style.display = "none";
-        let overlayOpen = false;
-    } else {
-        document.getElementById('helpOverlay').style.display = "block";
-        let overlayOpen = true;
-    }
+document.getElementById("helpForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (overlayOpen == true) {
+    document.getElementById("helpOverlay").style.display = "none";
+    let overlayOpen = false;
+  } else {
+    document.getElementById("helpOverlay").style.display = "block";
+    let overlayOpen = true;
+  }
 });
 // Close overlay btn
 function closeHelpOverlay() {
-    document.getElementById('helpOverlay').style.display = "none";
-    let overlayOpen = false;
+  document.getElementById("helpOverlay").style.display = "none";
+  let overlayOpen = false;
 }
 // Close overlay on background click
-//document.getElementById('helpOverlay').addEventListener('click', function (e) {
-//    e.stopPropagation();
-//    document.getElementById('helpOverlay').style.display = "none";
-//    let overlayOpen = false;
-//});
+document.getElementById("helpOverlay").addEventListener("click", function (e) {
+  e.stopPropagation();
+  document.getElementById("helpOverlay").style.display = "none";
+  let overlayOpen = false;
+});
