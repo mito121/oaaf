ac79746b3e5be69fca8b3b28006a299b34a29b38
AVPDesign <andreasvp@hotmail.dk>
2020-11-19 14:54:39 +0100

    noget js der ikke virker

--- assets/js/modules/how.js
+++ assets/js/modules/how.js
@@ -25,8 +25,34 @@ $(document).ready(function () {
     });
 });
 
-setInterval(imgChange, 5000)
+/* setInterval(function(){
 
-function imgChange(){
+let src1 = $(".how-img:nth-of-type(1)").attr();
+    if (src1 == "assets/img/info1_1"){
+        $(".how-img:nth-of-type(1)").attr("src", "assets/img/info1_2");
+    } else {
+        $(".how-img:nth-of-type(1)").attr("src", "assets/img/info1_1");
+    }
 
-}
+let src2 = $(".how-img:nth-of-type(2)").attr();
+    if (src2 == "assets/img/info2_1"){
+        $(".how-img:nth-of-type(2)").attr("src" "assets/img/info2_2");
+    } else {
+        $(".how-img:nth-of-type(2)").attr("src" "assets/img/info2_1");
+    }
+
+let src3 = $(".how-img:nth-of-type(3)").attr();
+    if (src3 == "assets/img/info3_1"){
+        $(".how-img:nth-of-type(3)").attr("src" "assets/img/info3_2");
+    } else {
+        $(".how-img:nth-of-type(3)").attr("src" "assets/img/info3_1");
+    }
+
+let src4 = $(".how-img:nth-of-type(4)").attr();
+    if (src4 == "assets/img/info4_1"){
+        $(".how-img:nth-of-type(4)").attr("src" "assets/img/info4_2");
+    } else {
+        $(".how-img:nth-of-type(4)").attr("src" "assets/img/info4_1");
+    }
+
+}, 5000); */
