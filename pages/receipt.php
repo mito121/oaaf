fda78d9036c0c98bfa9d9cf14b21671b44637a8d
AVPDesign <andreasvp@hotmail.dk>
2020-11-25 12:08:48 +0100

    lavet receipt lol

---
+++ pages/receipt.php
@@ -0,0 +1,26 @@
+<div class="global-wrapper">
+    <div class="sub-header">
+        <div class="back"></div>
+        <h1>Kvittering</h1>
+        <div class="spooky-ghost"></div>
+    </div>
+
+    <div class="row">
+        <div class="label">Nice</div>
+        <div class="sub-box toggle-collapse">
+            <div class="flex">
+                <p>lol</p><div class="expand rotate"></div>
+            </div>
+
+            <div class="collapsible">
+                <button class="wide-button">Overfør til saldo</button>
+                <p class="or-text">... eller indløs et gavekort</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="bg_temp">
+<img class="bg_temptation" src="assets/img/tmp_bg.svg" alt="background picture">
+</div>
