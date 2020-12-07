42d6a9c70d8565268666ff720eb2110b38d78f6d
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-12-07 12:46:31 +0100

    fixed delete profile
    kill session variables

--- handlers/deleteProfile.php
+++ handlers/deleteProfile.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 require_once '../includes/dbconnect.php';
 
@@ -8,6 +9,10 @@ $user_id = $_SESSION['user_id'];
 $sql = "DELETE FROM `oaaf_users` WHERE id = '$user_id'";
 $result = $conn->query($sql);
 
-header("Location: ../pages/login.php");
+// remove all session variables
+session_unset();
 
- ?>
+// destroy the session
+session_destroy();
+
+header("Location: ../pages/login.php");
